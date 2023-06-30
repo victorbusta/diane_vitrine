7dcf9c657e03dba8d071d993db162bb0e38068c9
victor <vic7991@gmail.com>
2023-06-30 12:19:28 +0200

    test for faster image loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import CheckoutView from '../views/CheckoutView.vue'
 import CustomerView from '@/views/CustomerView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
     },
     {
       path: '/checkout',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,6 @@ import * as anim from '@/utils.animation';
 const prints = ref<Print[]>([]);
 const modalPrint = ref<Print>();
 const modalOpened = ref(false);
-const printsUrl = ref<string[]>([]);
 const printsLoaded = ref(false);
 
 const fetchPrints = async () => {
@@ -42,39 +41,86 @@ const closeModal = async () => {
   modalOpened.value = false;
 }
 
+// const keyframes = [
+//         { opacity: 0, transform: 'translateY(100%)' },
+//         { opacity: 1, transform: 'translateY(0)' }
+//       ];
+
+// const showImg = (e: Event, printId: number) => {
+//   const parent: HTMLElement | null = (e.target as HTMLElement).parentElement;
+//   parent?.animate(keyframes, {
+//         duration: 500,
+//         fill: 'forwards',
+//         delay: 100 * printId,
+//         easing: 'ease',
+//         iterations: 1,
+//       });
+// }
+
 </script>
 
 <template>
-  <div class="wrapper">
+  <div class="wrapper" v-if="printsLoaded">
 
     <div class="image-gallery">
       <div v-for="print in evenPrints()" :key="print.id" class="image-container" @click="showModal(print)" >
-        <img :src="print.documentUrl">
+        <img id="img" :src="print.documentUrl" v-img-lazy>
+        <!-- <img id="img" :src="print.documentUrl" @load="$e => showImg($e, print.id)"> -->
       </div>
     </div>
 
     <div class="image-gallery">
       <div v-for="print in oddPrints()" :key="print.id" class="image-container" @click="showModal(print)" >
-        <img :src="print.documentUrl">
+        <img id="img" :src="print.documentUrl" v-img-lazy>
+
+        <!-- <img id="img" :src="print.documentUrl" @load="$e => showImg($e, print.id)"> -->
       </div>
-    </div>
+    </div> 
 
   </div>
 
-  <LoadingIcon style="width: 128px; height: 128px;" v-if="!printsLoaded"/>
+  <div v-else style="display: flex; flex-direction: column; align-items: center;">
+    <LoadingIcon style="width: 128px; height: 128px;"/>
+    <h1 style="text-align: center;">
+      Photos en cours de chargement
+    </h1>
+  </div>
 
   <div class="modal" >
     <CloseIcon height="64px" width="64px" style="z-index: 1;" @click="closeModal" :dark="false"/>
     <PrintCard v-if="modalOpened" :print="modalPrint as Print" />
   </div>
 
+
+  <hr>
+
+  <footer>
+    <h4 style="text-align: center;">
+    Les droits d'auteur des photos sont détenus par Van Butsele Diane et leur commercialisation n'est autorisée qu'en sa possession.
+    </h4>
+  </footer>
+
 </template>
 
 <style scoped>
+.refa-all{
+  width: 100%;
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
 .wrapper {
   display: flex;
   justify-content: center;
   align-items: start;
+}
+
+hr {
+  position: sticky;
+  top: 72px;
+  width: 90%;
+  margin: 0 5%;
+  stroke: var(--color-foreground);
 }
 
 .image-gallery {
@@ -88,7 +134,18 @@ const closeModal = async () => {
   width: 100%;
   height: fit-content;
   padding: 10px;
-  transition: all ease-in-out .2s;
+  /* opacity: 0;
+  transform: translateY(100%); */
+}
+
+@keyframes show {
+  100%{
+    opacity: 1;
+  }
+}
+
+.image-container-loaded {
+  opacity: 1;
 }
 
 .image-container:hover {
@@ -97,6 +154,7 @@ const closeModal = async () => {
 
 img {
   width: 100%;
+  transition: all .5s ease-in-out;
 }
 
 .modal {
@@ -115,7 +173,7 @@ img {
   background-color: aliceblue;
 }
 
-@media screen and (min-width: 1023px) {
+@media (min-width: 1023px) {
   .image-gallery {
     width: 30vw;
   }
